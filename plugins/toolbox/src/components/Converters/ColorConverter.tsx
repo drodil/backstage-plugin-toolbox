df4ab2d0cda71685fe4fb358908d08a925a83a57
Chris Langhout <clanghout@bol.com>
2023-11-22 11:03:59 +0100

    feat: add lab and lch color spaces

--- plugins/toolbox/src/components/Converters/ColorConverter.tsx
+++ plugins/toolbox/src/components/Converters/ColorConverter.tsx
@@ -11,7 +11,7 @@ import {
   Typography,
 } from '@material-ui/core';
 import * as colorConvert from 'color-convert';
-import { CMYK, HSV, RGB, HSL, KEYWORD, HEX } from 'color-convert/conversions';
+import { CMYK, HSV, RGB, HSL, KEYWORD, HEX, LAB, LCH } from 'color-convert/conversions';
 import { PasteFromClipboardButton } from '../Buttons/PasteFromClipboardButton';
 import { ClearValueButton } from '../Buttons/ClearValueButton';
 import { CopyToClipboardButton } from '../Buttons/CopyToClipboardButton';
@@ -25,6 +25,8 @@ export const ColorConverter = () => {
   const [hsv, setHsv] = React.useState<HSV>([0, 0, 0]);
   const [cmyk, setCmyk] = React.useState<CMYK>([0, 0, 0, 0]);
   const [html, setHtml] = React.useState<KEYWORD | null>(null);
+  const [lab, setLab] = React.useState<LAB>([0, 0, 0]);
+  const [lch, setLch] = React.useState<LCH>([0, 0, 0]);
   const sample = '#d50032';
 
   enum ColorType {
@@ -34,6 +36,8 @@ export const ColorConverter = () => {
     Hsv = 'HSV',
     Cmyk = 'CMYK',
     Html = 'HTML',
+    Lab = 'LAB',
+    Lch = 'LCH',
   }
 
   const getInputStr = () => input || '';
@@ -52,6 +56,10 @@ export const ColorConverter = () => {
         return ColorType.Cmyk;
       case colorConvert.keyword.rgb(color as KEYWORD)?.length === 3:
         return ColorType.Html;
+      case color.includes('lab'):
+        return ColorType.Lab;
+      case color.includes('lch'):
+        return ColorType.Lch;
       default:
         return null;
     }
@@ -65,6 +73,9 @@ export const ColorConverter = () => {
 
   const parseCmyk = ([c, m, y, k]: CMYK): string =>
     `cmyk(${c}%,${m}%,${y}%,${k}%)`;
+
+  const parseLab = ([l,a,b]: LAB): string => `lab(${l},${a},${b})`;
+  const parseLch = ([l,c,h]: LCH): string => `lch(${l},${c},${h})`;
 
   const removeCharacters = (value: string, colorType: string) => {
     return value
@@ -81,6 +92,8 @@ export const ColorConverter = () => {
     setHsv([0, 0, 0]);
     setCmyk([0, 0, 0, 0]);
     setHtml(null);
+    setLab([0, 0, 0]);
+    setLch([0, 0, 0]);
   };
 
   const handleHex = (value: string) => {
@@ -91,6 +104,8 @@ export const ColorConverter = () => {
       setHsv(colorConvert.hex.hsv(value));
       setCmyk(colorConvert.hex.cmyk(value));
       setHtml(colorConvert.hex.keyword(value));
+      setLab(colorConvert.hex.lab(value));
+      setLch(colorConvert.hex.lch(value));
     } catch (error) {
       handleNoMatch();
     }
@@ -108,6 +123,8 @@ export const ColorConverter = () => {
       setHsv(colorConvert.rgb.hsv(values));
       setCmyk(colorConvert.rgb.cmyk(values));
       setHtml(colorConvert.rgb.keyword(values));
+      setLab(colorConvert.rgb.lab(values));
+      setLch(colorConvert.rgb.lch(values));
     } catch (error) {
       handleNoMatch();
     }
@@ -124,6 +141,8 @@ export const ColorConverter = () => {
       setHsv(colorConvert.hsl.hsv(values));
       setCmyk(colorConvert.hsl.cmyk(values));
       setHtml(colorConvert.hsl.keyword(values));
+      setLab(colorConvert.hsl.lab(values));
+      setLch(colorConvert.hsl.lch(values));
     } catch (error) {
       handleNoMatch();
     }
@@ -140,6 +159,8 @@ export const ColorConverter = () => {
       setHsv(values);
       setCmyk(colorConvert.hsv.cmyk(values));
       setHtml(colorConvert.hsv.keyword(values));
+      setLab(colorConvert.hsv.lab(values));
+      setLch(colorConvert.hsv.lch(values));
     } catch (error) {
       handleNoMatch();
     }
@@ -157,6 +178,44 @@ export const ColorConverter = () => {
       setHsv(colorConvert.cmyk.hsv(values));
       setCmyk(values);
       setHtml(colorConvert.cmyk.keyword(values));
+      setLab(colorConvert.cmyk.lab(values));
+      setLch(colorConvert.cmyk.lch(values));
+    } catch (error) {
+      handleNoMatch();
+    }
+  };
+
+  const handleLab = (value: string) => {
+    const values: LAB = removeCharacters(value, 'lab')
+        .split(',')
+        .map((val: string) => parseInt(val, 10)) as LAB;
+
+    try {
+      setHex(`#${colorConvert.lab.hex(values)}`);
+      setRgb(colorConvert.lab.rgb(values));
+      setHsl(colorConvert.lab.hsl(values));
+      setHsv(colorConvert.lab.hsv(values));
+      setLab(values);
+      setLch(colorConvert.lab.lch(values));
+      setHtml(colorConvert.lab.keyword(values));
+    } catch (error) {
+      handleNoMatch();
+    }
+  };
+
+  const handleLch = (value: string) => {
+    const values: LCH = removeCharacters(value, 'lch')
+        .split(',')
+        .map((val: string) => parseInt(val, 10)) as LCH;
+
+    try {
+      setHex(`#${colorConvert.lch.hex(values)}`);
+      setRgb(colorConvert.lch.rgb(values));
+      setHsl(colorConvert.lch.hsl(values));
+      setHsv(colorConvert.lch.hsv(values));
+      setLab(colorConvert.lch.lab(values));
+      setLch(values);
+      setHtml(colorConvert.lch.keyword(values));
     } catch (error) {
       handleNoMatch();
     }
@@ -170,6 +229,8 @@ export const ColorConverter = () => {
       setHsv(colorConvert.keyword.hsv(value));
       setCmyk(colorConvert.keyword.cmyk(value));
       setHtml(value);
+      setLab(colorConvert.keyword.lab(value));
+      setLch(colorConvert.keyword.lch(value));
     } catch (error) {
       handleNoMatch();
     }
@@ -198,6 +259,12 @@ export const ColorConverter = () => {
       case ColorType.Html:
         handleHtml(value);
         break;
+      case ColorType.Lab:
+        handleLab(value);
+        break;
+      case ColorType.Lch:
+        handleLch(value);
+        break;
       default:
         setHex('');
         setRgb([0, 0, 0]);
@@ -205,6 +272,8 @@ export const ColorConverter = () => {
         setHsv([0, 0, 0]);
         setCmyk([0, 0, 0, 0]);
         setHtml(null);
+        setLab([0,0,0]);
+        setLch([0,0,0]);
         break;
     }
   };
@@ -259,6 +328,8 @@ export const ColorConverter = () => {
             <OutputField label="HSV" value={parseHsv(hsv)} />
             <OutputField label="CMYK" value={parseCmyk(cmyk)} />
             <OutputField label="HTML" value={html} />
+            <OutputField label="Lab" value={parseLab(lab)} />
+            <OutputField label="lch" value={parseLch(lch)} />
           </Grid>
           <Grid item lg={6} md={4} xs={12}>
             <Box bgcolor={hex} style={{ height: '50vh' }} />
