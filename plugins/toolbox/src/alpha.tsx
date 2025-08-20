75585be2f558c5f7521cb6a71d21e83db0e83b26
Hellgren Heikki <heikki.hellgren@op.fi>
2025-08-20 08:47:31 +0300

    chore: add support for new frontend system
    closes #167

---
+++ plugins/toolbox/src/alpha.tsx
@@ -0,0 +1,49 @@
+import {
+  ApiBlueprint,
+  createFrontendPlugin,
+  PageBlueprint,
+} from '@backstage/frontend-plugin-api';
+import {
+  compatWrapper,
+  convertLegacyRouteRef,
+  convertLegacyRouteRefs,
+} from '@backstage/core-compat-api';
+import { discoveryApiRef, fetchApiRef } from '@backstage/core-plugin-api';
+import { toolboxApiRef, ToolboxClient } from './api';
+import { rootRouteRef } from './routes.ts';
+
+const toolboxApi = ApiBlueprint.make({
+  params: defineParams =>
+    defineParams({
+      api: toolboxApiRef,
+      deps: {
+        discoveryApi: discoveryApiRef,
+        fetchApi: fetchApiRef,
+      },
+      factory({ discoveryApi, fetchApi }) {
+        return new ToolboxClient({ discoveryApi, fetchApi });
+      },
+    }),
+});
+
+const toolboxPage = PageBlueprint.make({
+  params: {
+    path: '/qeta',
+    routeRef: convertLegacyRouteRef(rootRouteRef),
+    loader: () =>
+      import('./components/Root').then(m => compatWrapper(<m.Root />)),
+  },
+});
+
+/**
+ * Backstage frontend plugin.
+ *
+ * @alpha
+ */
+export default createFrontendPlugin({
+  pluginId: 'toolbox',
+  routes: convertLegacyRouteRefs({
+    root: rootRouteRef,
+  }),
+  extensions: [toolboxApi, toolboxPage],
+});
