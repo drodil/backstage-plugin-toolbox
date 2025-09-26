92aab15925fc9dd6059f92f795537e57803f277e
Marcucella, Henry <Henry.Marcucella@fmr.com>
2025-09-26 15:26:15 -0400

    feat(sql-beautify): add dropdown to support languages
    Signed-off-by: Marcucella, Henry <Henry.Marcucella@fmr.com>

--- plugins/toolbox/src/components/Formatters/SQLBeautify.tsx
+++ plugins/toolbox/src/components/Formatters/SQLBeautify.tsx
@@ -1,30 +1,70 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import TextField from '@mui/material/TextField';
 import { DefaultEditor } from '../DefaultEditor';
 import { format } from 'sql-formatter';
 import { useToolboxTranslation } from '../../hooks';
+import { SQLLanguageSelector } from './SQLLanguageSelector';
 
+/**
+ * SQLBeautify - Formats SQL queries using sql-formatter.
+ * Allows selection of SQL language.
+ */
 export const SQLBeautify = () => {
+  const languages = useMemo(
+    () => [
+      'sql',
+      'bigquery',
+      'db2',
+      'db2i',
+      'hive',
+      'mariadb',
+      'mysql',
+      'n1ql',
+      'plsql',
+      'postgresql',
+      'redshift',
+      'singlestoredb',
+      'snowflake',
+      'spark',
+      'sqlite',
+      'transactsql',
+      'trino',
+    ],
+    [],
+  );
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
+  const [language, setLanguage] = useState('sql');
   const { t } = useToolboxTranslation();
 
   const sample = "SELECT bar, foo FROM foo_bar WHERE foo='bar' GROUP BY bar";
+  const debounceTime = 300;
 
   useEffect(() => {
-    let err;
-    try {
-      setOutput(format(input));
-      return;
-    } catch (e) {
-      err = e.message;
-    }
-
-    if (input && err) {
-      setOutput(err);
+    let handler: NodeJS.Timeout;
+    if (input) {
+      handler = setTimeout(() => {
+        try {
+          setOutput(format(input, { language }));
+        } catch (e) {
+          if (e instanceof Error && e.message) {
+            setOutput(e.message);
+          } else {
+            setOutput(
+              t('tool.format-sql.unexpectedError', {
+                defaultValue: 'An unexpected error occurred',
+              }),
+            );
+          }
+        }
+      }, debounceTime);
     } else {
       setOutput('');
     }
-  }, [input]);
+    return () => {
+      if (handler) clearTimeout(handler);
+    };
+  }, [input, language, t]);
 
   return (
     <DefaultEditor
@@ -39,6 +79,59 @@ export const SQLBeautify = () => {
       allowFileDownload
       downloadFileName="download.sql"
       downloadFileType="text/plain"
+      additionalTools={[
+        <SQLLanguageSelector
+          key="sqlLanguageSelector"
+          language={language}
+          onChange={setLanguage}
+          languages={languages}
+        />,
+      ]}
+      leftContent={
+        <TextField
+          name="editorSqlInput_abc1234"
+          label={t('tool.format-sql.inputLabel')}
+          id="input"
+          multiline
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          minRows={20}
+          variant="outlined"
+          autoComplete="new-password"
+          inputProps={{
+            form: 'off',
+            spellCheck: false,
+            autoComplete: 'new-password',
+          }}
+          sx={{
+            zIndex: 0,
+            width: '100%',
+            p: '8px',
+            '& label[class*="MuiFormLabel-root"]': {
+              paddingTop: '10px !important',
+              paddingLeft: '10px !important',
+            },
+          }}
+        />
+      }
+      rightContent={
+        <TextField
+          id="output"
+          label={t('tool.format-sql.outputLabel')}
+          value={output || ''}
+          multiline
+          minRows={20}
+          variant="outlined"
+          inputProps={{
+            spellCheck: false,
+            readOnly: true,
+          }}
+          sx={{
+            zIndex: 0,
+            width: '100%',
+          }}
+        />
+      }
     />
   );
 };
---
+++ plugins/toolbox/src/components/Formatters/SQLLanguageSelector.tsx
@@ -0,0 +1,53 @@
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useToolboxTranslation } from '../../hooks';
+import { useId } from 'react';
+
+interface SQLLanguageSelectorProps {
+  language: string;
+  onChange: (language: string) => void;
+  languages: string[];
+}
+
+/**
+ * SQLLanguageSelector - Dropdown for selecting SQL language.
+ */
+export const SQLLanguageSelector = ({
+  language,
+  onChange,
+  languages,
+}: SQLLanguageSelectorProps) => {
+  const { t } = useToolboxTranslation();
+  const getDisplayName = (lang: string) =>
+    t(`tool.format-sql.language.${lang}`, { defaultValue: lang });
+  const uniqueId = useId();
+
+  return (
+    <FormControl size="small" sx={{ minWidth: 175, mb: 1.5 }}>
+      <InputLabel
+        id={`sql-language-label-${uniqueId}`}
+        sx={{
+          zIndex: 0,
+        }}
+      >
+        Language
+      </InputLabel>
+      <Select
+        labelId={`sql-language-label-${uniqueId}`}
+        id={`sql-language-selector-${uniqueId}`}
+        value={language}
+        label="Language"
+        onChange={e => onChange(e.target.value)}
+        name={`sql-language-selector-${uniqueId}`}
+      >
+        {languages.map(m => (
+          <MenuItem key={m} value={m}>
+            {getDisplayName(m)}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
