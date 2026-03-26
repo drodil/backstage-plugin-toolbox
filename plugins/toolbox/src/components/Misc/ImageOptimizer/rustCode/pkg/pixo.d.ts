dc6e0c8769d0e63b4bf4d98ae1ee4352670b3516
Erfan Teymoori <erfan.teymoori@deutschebahn.com>
2026-03-26 18:39:54 +0100

    fix: resolve conflicts and update wasm files

--- plugins/toolbox/src/components/Misc/ImageOptimizer/rustCode/pkg/pixo.d.ts
+++ plugins/toolbox/src/components/Misc/ImageOptimizer/rustCode/pkg/pixo.d.ts
@@ -3,58 +3,23 @@
 
 export function init_hook(): void;
 
-export function pixo_compress(
-  input_data: Uint8Array,
-  qual: number,
-  img_type: string,
-): Uint8Array;
+export function pixo_compress(input_data: Uint8Array, qual: number, img_type: string): Uint8Array;
 
-export function pixo_resize(
-  input_data: Uint8Array,
-  resize_algorithm: string,
-  img_type: string,
-  dst_width: number,
-  dst_height: number,
-): Uint8Array;
+export function pixo_resize(input_data: Uint8Array, resize_algorithm: string, img_type: string, dst_width: number, dst_height: number): Uint8Array;
 
-export type InitInput =
-  | RequestInfo
-  | URL
-  | Response
-  | BufferSource
-  | WebAssembly.Module;
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly pixo_compress: (
-    a: number,
-    b: number,
-    c: number,
-    d: number,
-    e: number,
-  ) => [number, number, number, number];
-  readonly pixo_resize: (
-    a: number,
-    b: number,
-    c: number,
-    d: number,
-    e: number,
-    f: number,
-    g: number,
-    h: number,
-  ) => [number, number, number, number];
-  readonly init_hook: () => void;
-  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_malloc: (a: number, b: number) => number;
-  readonly __wbindgen_realloc: (
-    a: number,
-    b: number,
-    c: number,
-    d: number,
-  ) => number;
-  readonly __wbindgen_externrefs: WebAssembly.Table;
-  readonly __externref_table_dealloc: (a: number) => void;
-  readonly __wbindgen_start: () => void;
+    readonly memory: WebAssembly.Memory;
+    readonly pixo_compress: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+    readonly pixo_resize: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => [number, number, number, number];
+    readonly init_hook: () => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
@@ -67,9 +32,7 @@ export type SyncInitInput = BufferSource | WebAssembly.Module;
  *
  * @returns {InitOutput}
  */
-export function initSync(
-  module: { module: SyncInitInput } | SyncInitInput,
-): InitOutput;
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
 
 /**
  * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
@@ -79,9 +42,4 @@ export function initSync(
  *
  * @returns {Promise<InitOutput>}
  */
-export default function __wbg_init(
-  module_or_path?:
-    | { module_or_path: InitInput | Promise<InitInput> }
-    | InitInput
-    | Promise<InitInput>,
-): Promise<InitOutput>;
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
